991fd66d1f938c0068eadfbc8352249a4a51dffd
mathpss <matheus.p.souza.silva@gmail.com>
2024-11-21 21:16:11 -0300

    Desafio sobre POO concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,14 +2,25 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            //Implementado!!!!!!!!!!
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
+
+        public string Numero { get; set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        //Implementado!!!!!!!!!!
+        protected string Modelo;
+
+        protected string Imei;
+
+        protected int Memoria;
+        
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia (numero: "995959595", modelo: "Modelo1", imei: "111111", memoria: 128);
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "987878787", modelo: "Modelo2", imei: "222222", memoria: 32);
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("Tinder");
+
+// nokia.Modelo //<- testando se realmente a propriedade está protegida 
